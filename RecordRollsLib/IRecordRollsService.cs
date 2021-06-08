0be24e14d813be00123a9b3b63219646a28df736
Christina Lukaszczyk <clukas93@gmail.com>
2021-06-08 04:46:38 -0400

    Added RecordRollsLib for WCF; also renamed "MainWindow" to "DiceRollerView"

---
+++ RecordRollsLib/IRecordRollsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RecordRollsLib
+{
+    [ServiceContract]
+    public interface IRecordRollsService
+    {
+        [OperationContract]
+        void RecordRoll(string rollStr);
+
+        [OperationContract]
+        List<string> GetRollRecord();
+
+        [OperationContract]
+        void SaveRollRecord();
+    }
+}
---
+++ RecordRollsLib/RecordRollsService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RecordRollsLib
+{
+    public class RecordRollsService : IRecordRollsService
+    {
+        private List<string> _rollList;
+
+        /// <summary>
+        /// Adds rollStr to _rollList
+        /// </summary>
+        /// <param name="rollStr">The last roll (ex. "2 + 4 + 1")</param>
+        public void RecordRoll(string rollStr)
+        {
+            _rollList.Add(rollStr);
+        }
+
+        /// <summary>
+        /// Returns list of previous rolls
+        /// </summary>
+        /// <returns>_rollList</returns>
+        public List<string> GetRollRecord()
+        {
+            return _rollList;
+        }
+
+        /// <summary>
+        /// Saves _rollList in a text file
+        /// </summary>
+        public void SaveRollRecord()
+        {
+            // TK to implement
+        }
+    }
+}
